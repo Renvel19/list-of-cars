215038545c426fe49052ec237953136b24dbc183
Renvel19 <af.renteria2@uniandes.edu.co>
2024-04-26 12:29:21 -0500

    add cars by brand table

--- src/app/cars/components/cars/cars.component.ts
+++ src/app/cars/components/cars/cars.component.ts
@@ -9,16 +9,36 @@ import { Car } from '../../models/car';
 })
 export class CarsComponent {
   cars: Array<Car> = [];
+  carsBybrand: Array<[string, number]> = [];
 
   constructor(private carsService: CarsService) {}
 
   ngOnInit() {
     this.getCars();
+    this.searchCarsByBrand();
   }
 
   getCars() {
     this.carsService.getCars().subscribe((cars) => {
       this.cars = cars;
     });
+  }
+
+  searchCarsByBrand() {
+    for (const car of this.cars) {
+      let foundedCar = this.carsBybrand.find(
+        (carBrand) => carBrand[0] === car.marca
+      );
+      if (
+        this.carsBybrand.find((carBrand) => carBrand[0] === car.marca) !=
+        undefined
+      ) {
+        foundedCar![1] += 1;
+      } else {
+        this.carsBybrand.push([car.marca, 1]);
+      }
+    }
+    this.carsBybrand.sort((a, b) => b[1] - a[1]);
+    console.log(this.carsBybrand);
   }
 }
